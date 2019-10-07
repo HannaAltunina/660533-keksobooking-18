877b22aa690a5b0667c3ee02f50d7bc27ed7e827
Hanna Altunina <hanna.aljaxeeva@gmail.com>
2019-10-07 19:28:46 +0300

    пождвижность пина

--- js/main.js
+++ js/main.js
@@ -56,10 +56,57 @@
 
   window.main.pageDeactivation();
 
-  window.mainPin.addEventListener('mousedown', window.main.pageActivation);
+  window.mainPin.addEventListener('click', window.main.pageActivation);
   window.mainPin.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 13) {
       window.main.pageActivation();
     }
+  });
+
+  window.mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.mainPin.style = 'left: ' + (window.mainPin.offsetLeft - shift.x) + 'px; top: ' + (window.mainPin.offsetTop - shift.y) + 'px;';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      window.data.getAddressCoordinate();
+
+      if (dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          window.mainPin.removeEventListener('click', onClickPreventDefault);
+        };
+        window.mainPin.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
   });
 })();
