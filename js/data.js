ff16dab228bdc110d1a5ce48dc6a57ae9fcb9245
Hanna Altunina <hanna.aljaxeeva@gmail.com>
2019-10-22 16:55:13 +0300

    окна ошибки и успеха при отправке данных формы на сервер

--- js/data.js
+++ js/data.js
@@ -4,7 +4,9 @@
   var ESC_KEYCODE = 27;
   var main = document.querySelector('main');
   var error = document.querySelector('#error').content.querySelector('.error');
+  var success = document.querySelector('#success').content.querySelector('.success');
   var map = document.querySelector('.map');
+  var form = document.querySelector('.ad-form');
 
 
   function onDataLoad(propositions) {
@@ -18,37 +20,63 @@
     document.querySelector('.error').classList.add('hidden');
   }
 
+  function closeSuccess() {
+    document.querySelector('.success').classList.add('hidden');
+  }
+
   function onErrorEscPress(evt) {
     if (evt.keyCode === ESC_KEYCODE) {
       closeError();
     }
   }
 
-  function onAnyFieldClick(evt) {
+  function onSuccessEscPress(evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeSuccess();
+    }
+  }
+
+  function onAnyErrorFieldClick(evt) {
     if (evt.target !== document.querySelector('.error__message')) {
       closeError();
     }
   }
 
-  function renderError(element) {
-    var errorElement = element.cloneNode(true);
+  function onAnySuccessFieldClick(evt) {
+    if (evt.target !== document.querySelector('.success__message')) {
+      closeSuccess();
+    }
+  }
+
+
+  function renderInfoMessage(element) {
+    var messageElement = element.cloneNode(true);
     var fragment = document.createDocumentFragment();
 
-    fragment.appendChild(errorElement);
-    errorElement.querySelector('.error__button').addEventListener('click', closeError);
-    document.addEventListener('click', onAnyFieldClick);
-    document.addEventListener('keydown', onErrorEscPress);
+    fragment.appendChild(messageElement);
 
     return fragment;
   }
 
   function onError() {
-    var errorMessage = renderError(error);
+    var errorMessage = renderInfoMessage(error);
+    errorMessage.querySelector('.error__button').addEventListener('click', closeError);
+    document.addEventListener('click', onAnyErrorFieldClick);
+    document.addEventListener('keydown', onErrorEscPress);
+
     if (document.querySelector('.error')) {
       return;
     } else {
       main.appendChild(errorMessage);
     }
+  }
+
+  function onSuccess() {
+    var successMessage = renderInfoMessage(success);
+    document.addEventListener('click', onAnySuccessFieldClick);
+    document.addEventListener('keydown', onSuccessEscPress);
+    main.appendChild(successMessage);
+    form.reset();
   }
 
   function init() {
@@ -57,7 +85,10 @@
 
   window.data = {
     ESC_KEYCODE: ESC_KEYCODE,
+    onError: onError,
+    onSuccess: onSuccess,
     map: map,
-    init: init
+    init: init,
+    form: form
   };
 })();
--- js/form.js
+++ js/form.js
@@ -10,11 +10,12 @@
   var timeoutSelect = document.querySelector('#timeout');
   var submit = document.querySelector('.ad-form__submit');
 
+
   function checkRooms() {
     var roomsSelectedOption = roomSelect.options[roomSelect.selectedIndex].value;
     var capacitySelectedOption = capacitySelect.options[capacitySelect.selectedIndex].value;
 
-    if (capacitySelectedOption > roomsSelectedOption) {
+    if (parseInt(capacitySelectedOption, 10) > parseInt(roomsSelectedOption, 10)) {
       capacitySelect.setCustomValidity('Количество гостей не должно превышать количество комнат');
     } else {
       capacitySelect.setCustomValidity('');
@@ -33,7 +34,7 @@
     var priceConform = window.util.getConformity(typeSelectedOption, window.card.PROPERTY_TYPES, MIN_PRICES);
     var translateType = window.util.getConformity(typeSelectedOption, window.card.PROPERTY_TYPES, window.card.TRANSLATE_PROPERTIES);
 
-    if (priceInput.value < priceConform) {
+    if (parseInt(priceInput.value, 10) < parseInt(priceConform, 10)) {
       priceInput.setCustomValidity('Минимальная стоимость проживания за ночь ' + priceConform + ' в объекте ' + translateType);
     } else {
       priceInput.setCustomValidity('');
@@ -55,7 +56,16 @@
   capacitySelect.addEventListener('change', checkRooms);
   roomSelect.addEventListener('change', checkRooms);
   priceInput.addEventListener('change', checkPropertyPrices);
-  typeSelect.addEventListener('change', changePricePlaceholder);
+  typeSelect.addEventListener('change', changePricePlaceholder, checkPropertyPrices);
 
   submit.addEventListener('click', checkRooms, checkPropertyPrices);
+
+  window.data.form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(window.data.form), window.data.onSuccess, window.data.onError);
+    evt.preventDefault();
+  });
+
+  window.form = {
+
+  };
 })();
